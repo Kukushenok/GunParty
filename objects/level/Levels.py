662154fed7195390a2acd43196f5c3656f5c7950
IlyaP <mrcoolmoder@gmail.com>
2018-02-24 21:30:19 +0300

    Добавлены презентация и видео

--- objects/level/Levels.py
+++ objects/level/Levels.py
@@ -7,7 +7,7 @@ class Levels:
         self.levelRoot = os.path.join(os.getcwd(), "resources", "levels")
         for e in [os.path.join(self.levelRoot, o) for o in os.listdir(self.levelRoot)]:
             if os.path.isdir(e):
-                self.addLevel(e.split("\\")[-1],e)
+                self.addLevel(e.split("/")[-1],e)
 
     def addLevel(self,name,path):
         self.levels[name] = level.Level(self.levelRoot, path)
